c931197d3e45f1f395299aa3a36dfc45e51c419a
Davion <daviongarcia@gmail.com>
2022-01-27 18:26:19 -0800

    api links

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
+import imp
 from flask import Flask, request, abort
 from data import catalog
 import json
 import random
 from config import db
 from flask_cors import CORS
+from bson import ObjectId
 
 app = Flask('Main')
 CORS(app)
@@ -38,42 +40,60 @@ def about():
 @app.route('/api/catalog')
 def get_catalog():
 
-    test = db.products.find({})
-    print(test)
+    cursor = db.products.find({})
+    print(cursor)
 
-    return json.dumps(catalog)
+    prods = []
+    for prod in cursor:
+      prod['_id'] = str(prod['_id'])
+      prods.append(prod)
+
+    return json.dumps(prods)
 
 
 @app.route('/api/catalog', methods=['post'])
 def save_prod():
     prod = request.get_json()
+    print(prod)
+    # if not 'title' in prod or len(prod['title']) < 5:
+    #     return abort(400, '"Title" required, and must be a least 5 chars')
 
-    if not 'title' in prod or len(prod['title']) < 5:
-        return abort(400, '"Title" required, and must be a least 5 chars')
+    # if not 'price' in prod or prod['price'] < 0:
+    #     return abort(400, '"Price" required, and must be > 0')
 
-    if not 'price' in prod or prod['price'] < 0:
-        return abort(400, '"Price" required, and must be > 0')
+    # prod['_id'] = random.randint(1000, 100000)
+    # catalog.append(prod)
 
-    prod['_id'] = random.randint(1000, 100000)
-    catalog.append(prod)
+    db.products.insert_one(prod)
 
-    return 'ok'
+    prod['_id'] = str(prod['_id'])
+
+    return json.dumps(prod)
 
 
 @app.route('/api/cheapest')
 def get_cheapest():
-    cheap = catalog[0]
-    for prod in catalog:
+    cursor = db.products.find({})
+    cheap = cursor[0]
+    for prod in cursor:
         if prod['price'] < cheap['price']:
             cheap = prod
+    
+    cheap['_id'] = str(cheap['_id'])    
     return json.dumps(cheap)
 
 
 @app.route('/api/product/<id>')
 def get_id(id):
-    for prod in catalog:
-        if prod['_id'] == id:
-            return prod
+
+    if(not ObjectId.is_valid(id)):
+      return abort(400, 'ID is not Valid')
+    results = db.products.find_one({'_id': ObjectId(id)})
+    if not results:
+      return abort(404)
+    results['_id'] = str(results['_id'])    
+    
+    return json.dumps(results)
 
 
 @app.route('/api/catalog/<cat>')
